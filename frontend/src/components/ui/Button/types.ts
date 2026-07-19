51b247d27f75d1cca106dab6f90a9216d76e4b18
Wygna79 <wygna.vitoria@escolar.ifrn.edu.br>
2026-07-19 08:00:08 -0300

    alteração 24.0

---
+++ frontend/src/components/ui/Button/types.ts
@@ -0,0 +1,11 @@
+import type { ButtonHTMLAttributes } from "react";
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+
+  variant?: "primary" | "secondary" | "danger";
+
+  fullWidth?: boolean;
+
+  loading?: boolean;
+}
